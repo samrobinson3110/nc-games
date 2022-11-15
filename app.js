c80bcd0f9812bda01d4f838f75fc1b301126f574
Sam Robinson <sxr969@student.bham.ac.uk>
2022-11-15 14:18:50 +0000

    Altered error handler

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use((err, req, res, next) => {
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
   }
 });
 
